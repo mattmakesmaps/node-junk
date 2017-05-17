21351faa5bbf7dba2f1e3b48ae8f308b83291916
Matthew Kenny <matthewkenny@gmail.com>
2017-05-17 08:21:37 -0700

    Trying `bind()` and `through()`

--- piper.js
+++ piper.js
@@ -3,6 +3,8 @@ var inherits = require('util').inherits;
 var EventEmitter = require('events').EventEmitter;
 var inside = require('point-in-polygon');
 var iss = require('iss');
+// Converts "classic" style streams to pipeable v2 streams.
+var through = require('through');
 
 module.exports = Piper;
 
@@ -26,14 +28,34 @@ Piper.prototype.pip = function(inPoint) {
 }
 
 Piper.prototype.trackISS = function() {
-    // `self` can be passed to callback of stream.on('data') event
-    // to give access to Piper methods.
-    var self = this;
     var stream = iss.locationStream(25544, 10);
     
-    stream.on('data', function (buffer) {
+    /**
+     * Begin Stream V1 Example, which passes context using `var self = this;`
+     * 
+     * var self = this;
+     * stream.on('data', function (buffer) {
+     *   var rawJson = buffer.toString('utf8')
+     *     var data = JSON.parse(rawJson);
+     *     selfpip([data.longitude, data.latitude]);
+     * }); 
+     */
+
+    /**
+     * Stream V2 API using `through`
+     */
+
+    // Note use of `bind` to pass context
+    stream.pipe(through(read.bind(this), end));
+
+    function read(buffer) {
         var rawJson = buffer.toString('utf8')
         var data = JSON.parse(rawJson);
-        self.pip([data.longitude, data.latitude]);
-    });
+        this.pip([data.longitude, data.latitude]);
+    }
+
+    function end() {
+        console.log('end');
+    }
+
 }
